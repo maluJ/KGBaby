b435514140debacad87dc486d07107659c0eb87d
tim <tsandy23@gmail.com>
2025-03-13 21:50:51 +0100

    add install scripts, image streamer and update readme

--- README.md
+++ README.md
@@ -4,3 +4,12 @@
 <p>
   <img src="img/Initial_Meeting_Notes.jpeg" width="600" title="initial meeting">
 </p>
+
+
+## Setup Python schtuff
+Run the following to set up and activate the Python venv for streaming data from the various devices.
+1. `chmod +x scripts/install_pyenv.sh`
+2. `./scripts/install_pyenv.sh`
+3. `chmod +x scripts/setup_venv.sh`
+4. `./scripts/setup_venv.sh`
+5. `pyenv activate kgbaby`
---
+++ scripts/setup_venv.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init --path)"
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
+
+sudo apt update; sudo apt install build-essential libssl-dev zlib1g-dev \
+libbz2-dev libreadline-dev libsqlite3-dev curl git \
+libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
+
+pyenv install 3.12.9
+pyenv virtualenv 3.12.9 kgbaby
+pyenv activate kgbaby
+
+pip install opencv-python
+
