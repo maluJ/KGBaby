265b193ef5e57c56db273b3f5774ac44b9975e8e
tim <tsandy23@gmail.com>
2025-03-13 22:11:14 +0100

    add audio recorder script. ignore data directory

--- scripts/setup_venv.sh
+++ scripts/setup_venv.sh
@@ -8,11 +8,11 @@ eval "$(pyenv virtualenv-init -)"
 
 sudo apt update; sudo apt install build-essential libssl-dev zlib1g-dev \
 libbz2-dev libreadline-dev libsqlite3-dev curl git \
-libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
+libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev portaudio19-dev
 
 pyenv install 3.12.9
 pyenv virtualenv 3.12.9 kgbaby
 pyenv activate kgbaby
 
-pip install opencv-python
+pip install opencv-python pyaudio
 
