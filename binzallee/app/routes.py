f789bd28c0300445ed2a0611e45eabaa4ad4038f
Hampus Malmberg <hampus.malmberg88@gmail.com>
2025-03-15 17:29:29 +0100

    sound output

--- binzallee/app/routes.py
+++ binzallee/app/routes.py
@@ -1,8 +1,24 @@
 from flask import render_template
 from . import create_app
+import json
+import os
 
 app = create_app()
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/event_log')
+def event_log():
+    with open('event_data.json') as f:
+        data = json.load(f)
+    return data
+
+@app.route('/action', methods=['POST'])
+def action():
+    # Handle the POST request here
+    print('Action received')
+    os.system('aplay proud-fart-288263.wav')
+    return 'Action received'
+    
